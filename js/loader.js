e2d2af522409780def354aa8c027248e0b004622
BruhJam <129856681+BruhJam@users.noreply.github.com>
2026-04-07 02:27:42 +0530

    First

---
+++ js/loader.js
@@ -0,0 +1,18 @@
+
+async function loadComponent(id, file){
+    const res = await fetch(file);
+    const html = await res.text();
+    document.getElementById(id).innerHTML = html;
+}
+
+document.addEventListener("DOMContentLoaded", ()=>{
+
+    if(document.getElementById("header")){
+        loadComponent("header","/components/header.html");
+    }
+
+    if(document.getElementById("footer")){
+        loadComponent("footer","/components/footer.html");
+    }
+
+});
---
+++ js/nav.js
@@ -0,0 +1,15 @@
+
+function initBackButton(){
+const backButton=document.getElementById("backButton");
+if(!backButton) return;
+
+if(history.length<=1){
+backButton.style.display="none";
+}
+
+backButton.addEventListener("click",()=>{
+history.back();
+});
+}
+
+document.addEventListener("DOMContentLoaded",initBackButton);
